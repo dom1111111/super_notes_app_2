24d9a4d3f99c3fd96b32f4101bb1f6f16802e660
dom127 <dom603@protonmail.com>
2023-03-15 10:47:28 -0400

    starting again to refactor code and change program structure

--- commands.py
+++ commands.py
@@ -1,83 +1,60 @@
 from time import sleep
 from datetime import datetime
-#from external_scripts.vosk_trancriber import VoskT
-from external_scripts.whisper_transcriber import WhisperT
 
-import GUI_tk as GUI
-import stt_engine
-import play_rec_audio
+from external_scripts import stt_engine, play_rec_audio, whisper_transcriber #, VoskT
+from external_scripts import GUI_tk as GUI
+
+#-------------------------------
+# Common shared functions
+
+time_str_format_1 = '%Y%m%d%H%M%S%f'
+common_keywords = {                # even single word combos MUST be tuples - don't foget the comma
+    'app_command_word': ('computer', ),
+    'get':      ('get', 'return', 'retrieve', 'show', 'display', 'read'),
+    'search':   ('search', 'find', 'seek', 'look'),
+    'create':   ('create','make', 'new'),   # 'write', 'start', 'compose'
+    'exit':     ('exit', 'shutdown', 'shut down', 'terminate', 'stop', 'bye', 'goodbye', 'good bye'),
+    'end':      ('end', 'finish', 'complete'),
+    'app':      ('app', 'application', 'program', 'computer'),
+    'note':     ('note', 'text', 'entry', 'page'),
+    'task':     ('task', 'todo', 'to-do'),
+    'recent':   ('recent', 'latest', 'last'),
+    'current':  ('current', 'present'),
+    'today':    ('today', 'todays', "today's")
+}
+
+# check if the specified keywords are present in text
+def match_keywords(keyword_names:tuple, text:str):
+    match_count = 0
+    for key in keyword_names:
+        keywords = common_keywords.get(key)
+        for word in keywords:
+            if word in text.lower():
+                match_count += 1
+                break
+    # only return True if each keyword group gets at least one match
+    if match_count >= len(keyword_names):
+        return True
+
+def validate_if_within_timeout(current_time:datetime, last_time:datetime, timeout):
+    if (current_time - last_time).seconds <= timeout:
+        return True
 
 #-------------------------------
 
-class AppCommands:
+class CoreFunctions:
     def __init__(self):
         self.loop = False
-        self.sub_progs = []                     # should store active program objects
-        self.vox = stt_engine.VoiceToText(WhisperT)
-        
-        self.time_str_format_1 = '%Y%m%d%H%M%S%f'
-        self.common_keywords = {                # even single word combos MUST be tuples - don't foget the comma
-            'app_command_word': ('computer', ),
-            'get':      ('get', 'return', 'retrieve', 'show', 'display', 'read'),
-            'search':   ('search', 'find', 'seek', 'look'),
-            'create':   ('create','make', 'new'),   # 'write', 'start', 'compose'
-            'exit':     ('exit', 'shutdown', 'shut down', 'terminate', 'stop', 'bye', 'goodbye', 'good bye'),
-            'end':      ('end', 'finish', 'complete'),
-            'app':      ('app', 'application', 'program', 'computer'),
-            'note':     ('note', 'text', 'entry', 'page'),
-            'task':     ('task', 'todo', 'to-do'),
-            'recent':   ('recent', 'latest', 'last'),
-            'current':  ('current', 'present'),
-            'today':    ('today', 'todays', "today's")
-        }
+        self.sub_progs = []                             # should store active sub-program objects
 
-    #---------
-    # General Functions
-
-    # check if the specified keywords are present in text
-    def match_keywords(self, keyword_names:tuple, text:str):
-        match_count = 0
-        for key in keyword_names:
-            keywords = self.common_keywords.get(key)
-            for word in keywords:
-                if word in text.lower():
-                    match_count += 1
-                    break
-        # only return True if each keyword group gets at least one match
-        if match_count >= len(keyword_names):
-            return True
-
-    def validate_if_within_timeout(self, current_time:datetime, last_time:datetime, timeout):
-        if (current_time - last_time).seconds <= timeout:
-            return True
-
-    #---------
-    # System Functions
-
-    def run_GUI(self):
-        GUI.run_GUI()   # THIS MUST BE CALLED ONCE, AND WILL PERSIST
-
-    def start_core_procs(self):
-        self.nl_print('loading voice input...')
-        self.vox.start_listening()
-        self.nl_print('voice input now active!')
-
-    def end_core_procs(self):
-        self.nl_print('shutting down...')
-        self.nl_print('ending voice input...')
-        self.vox.stop_listening()
-        sleep(1)
-        self.nl_print('voice input now off')
-        GUI.terminate_GUI()
-
-    def end_loop(self):
-        self.loop = False
-    
     def redo(self):
         pass
 
     def undo(self):
         pass
+
+    def end_loop(self):
+        self.loop = False
 
     def spawn_sub_program(self, program_object, name:str):
         self.nl_print(f'starting sub_program: "{name}"')
@@ -85,21 +62,58 @@ class AppCommands:
         self.sub_progs.append(prog)                     # add object to sub_progs list
         self.sub_progs.sort(key=lambda x: x.priority)   # sort sub_progs list by object attribute: "priority"
 
-    def terminate_sub_program(self, name_id:str):
+    def terminate_sub_program(self, id:str):
         for prog in self.sub_progs:
-            if prog.name_id == name_id:
+            if prog.id == id:
                 i = self.sub_progs.index(prog)
                 self.sub_progs.pop(i)
                 break
 
-    #---------
-    #IO Funcs
+#-------------------------------
 
-    def get_voice_input(self):
-        return self.vox.get_transcribed_phrase()
+class InputOutput:
+    def __init__(self):
+        self.vox = stt_engine.VoiceToText(whisper_transcriber.WhisperT)
 
     def nl_print(self, x:str=None):
         print('\n'+x)
+
+    #---------
+    # Voice input from mic
+
+    def start_voice_input(self):
+        self.vox.start_listening()
+
+    def get_voice_input(self):
+        return self.vox.get_transcribed_phrase()
+    
+    #def get_current_input_text(self):
+    #    pass
+
+    def end_voice_input(self):
+        self.vox.stop_listening()
+
+    def use_full_vocabulary(self):
+        pass
+
+    def set_vocabulary(self, words:tuple):
+        pass
+    
+    def add_to_vocabulary(self, words:tuple):
+        pass
+
+    def reset_vocabulary(self):
+        pass
+
+    #---------
+    # GUI
+
+    def start_GUI(self):
+        """this must be called from the main thread, and will persist - will not return!"""
+        GUI.run_GUI()
+
+    def end_GUI(self):
+        GUI.terminate_GUI()
 
     def output_to_log(self, message:str):
         GUI.append_to_log(message)
--- main.py
+++ main.py
@@ -7,6 +7,84 @@ Coms = commands.coms
 
 #-------------------------------
 
+class Main():
+    def __init__(self):
+
+        # for voice input validation
+        self.last_wake_time = None
+        self.wake_timeout = 5       # in seconds
+
+        # THIS SHOULD EVENTUALLY BE A JSON
+        # with open('com_ref.json', 'w') as file:
+        #     self.command_reference = json.loads(file)
+        # Use this for now:
+        self.command_reference = [
+            {
+                'name':         'new note',
+                'conditions':   [(Coms.match_keywords, ('create', 'note'))],
+                'command':      (Coms.spawn_sub_program, (CreateNoteQuick, 'new note'))
+            },
+            {
+                'name':         'shutdown',
+                'conditions':   [(Coms.match_keywords, ('exit', 'app'))],
+                'command':      (Coms.end_loop, None)
+            }
+        ]
+
+    def main(self):
+        # only run this function if transcription is not None
+        if not transcription:
+            return
+        # get variables from input
+        self.unpack_transcription(transcription)
+
+        # [1] first validate input for command activation
+        # A. validate that either the phrase contains a wake word, or it happened within the wake_timeout
+        if Coms.match_keywords(('app_command_word', ), self.text):
+            pass
+        elif self.last_wake_time and Coms.validate_if_within_timeout(self.time, self.last_wake_time, self.wake_timeout):
+            pass
+        else:
+            return
+        
+        # if all the above conditions are met, then set the wake time to now, and continue
+        self.last_wake_time = datetime.now()
+        
+        # [2] try getting action from input
+        for command in self.command_reference:
+            name = command.get('name')
+            conditions = command.get('conditions')
+            command = command.get('command')
+            
+            # check each of the comand's conditions - if any fail, then break the loop
+            for condition in conditions:
+                condition_func = condition[0]
+                condition_arg = condition[1]
+                passed = condition_func(condition_arg, self.text)
+                if not passed:
+                    break
+            
+            # if all of the conditions pass, then do the action
+            if passed:
+                func = command[0]
+                args = command[1]
+
+                Coms.nl_print(f'starting command: {name}')
+                self.last_wake_time = None      # reset wake time
+
+                if args:
+                    # check if there is more than one argument, by seeing if args is an iterable
+                    if hasattr(args, '__iter__'):
+                        func(*args)
+                    else:
+                        func(args)
+                else:
+                    func()
+                
+                return True
+        return
+
+
 def main_loop():
     while Coms.loop:
         # [1] check for input
@@ -31,10 +109,25 @@ def main_loop():
         # append command stuff to the log
 
     # shutdown
-    Coms.end_core_procs()
+    """
+        def end_core_procs(self):
+            self.nl_print('shutting down...')
+            self.nl_print('ending voice input...')
+            self.vox.stop_listening()
+            sleep(1)
+            self.nl_print('voice input now off')
+            GUI.terminate_GUI()
+    """
 
 def run():
     # start
+    """
+    def start_core_procs(self):
+        self.nl_print('loading voice input...')
+        self.vox.start_listening()
+        self.nl_print('voice input now active!')
+    """
+    # set the transcriber vocabulary
     Coms.loop = True
     Coms.spawn_sub_program(sub_programs.VoiceController, 'voice controller')    # launch the voice controller, which will check input for new commands
     Coms.start_core_procs()
