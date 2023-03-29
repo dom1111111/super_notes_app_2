3c8ba0e226185e876072f49e866a2390e68a3659
dom127 <dom603@protonmail.com>
2023-03-28 23:44:13 -0400

    refactoring. Almost done main.py main loop. Completed basic SharedResourceWrapper in system_tools.py, refactored and wrapped methods in each static class

--- main.py
+++ main.py
@@ -7,156 +7,142 @@ import programs_commands as PC
 
 #-------------------------------
 
-class Main:
-    loop = False
-    # active sub-program objects
-    progs = {}
-    current_prog_focus = None
-    # for voice input validation
-    last_wake_time = None
-    wake_timeout = 5       # in seconds
-    # stores all commands which can be done by input as the base level
-    input_command_reference = [
-        {
-            'name':         'New Note',
-            'condition':    ('create', 'note'),
-            'command':      PC.CreateNoteQuick
-        },
-        {
-            'name':         'Calculator',
-            'condition':    (),
-            'command':      PC.Calculator
-        },
-        {
-            'name':         'Shutdown',
-            'condition':    ('exit', 'app'),
-            'command':      shutdown
-        }
-        # undo
-        # redo
-    ]
+class MainProgram:
+    def __init__(self):
+        self.loop = False
+        # active sub-program objects
+        self.progs = {}
+        self.current_prog_focus = None
+        # for voice input validation
+        self.wake_words = "computer"
+        self.last_wake_time = None
+        self.wake_timeout = 5       # in seconds
+        # stores all commands which can be done by input as the base level
+        self.input_command_reference = [
+            {
+                'name':         'New Note',
+                'condition':    ('create', 'note'),
+                'command':      (self.spawn_program, PC.CreateNoteQuick)
+            },
+            {
+                'name':         'Calculator',
+                'condition':    (),
+                'command':      (self.spawn_program, PC.Calculator)
+            },
+            {
+                'name':         'Shutdown',
+                'condition':    ('exit', 'app'),
+                'command':      (self.shutdown, None)
+            }
+            # undo
+            # redo
+        ]
+        self.command_keyword_list = #[for words in ]
 
     #---------
 
-    @classmethod
-    def main_loop(cls):
-        while cls.loop:
-            # [1] check for input - restart loop if none
-            input_package = ST.VoiceInput.get_transcribed_phrase()
-            if not input_package:
-                continue
-            input_text = input_package.get('text')
+    def spawn_program(self, program_object, name:str):
+        ST.TerminalOutput.nl_print(f'starting sub_program: "{name}"')
+        prog = program_object(name)                     # instatiate the object
+        prog_id = prog.id
+        self.progs.update({prog_id: prog})                   # add object to sub_progs list
+
+    def terminate_program(self, id:str):
+        prog = self.progs.get(id)
+        prog.end()
+        self.progs.pop(id)
+
+    # check conditions, spawn commands
+    def check_spawn_commands(self, text):
+        # [2] try getting action from input
+        for command in self.input_command_reference:
+            name = command.get('name')
+            conditions = command.get('conditions')
+            command = command.get('command')
             
-            ST.TerminalOutput.nl_print(f'input: "{input_text}"')
+            # check each of the comand's conditions - if any fail, then break the loop
+            for condition in conditions:
+                condition_func = condition[0]
+                condition_arg = condition[1]
+                passed = condition_func(condition_arg, text)
+                if not passed:
+                    break
+            
+            # if all of the conditions pass, then do the action
+            if passed:
+                func = command[0]
+                args = command[1]
 
-            # [2] run main program and pass it input
-            main = self.main_program(input_package)
-            # check if it uses input
-            if main:
+                ST.TerminalOutput.nl_print(f'starting command: {name}')
+                last_wake_time = None      # reset wake time
+
+                if args:
+                    # check if there is more than one argument, by seeing if args is an iterable
+                    if hasattr(args, '__iter__'):
+                        func(*args)
+                    else:
+                        func(args)
+                else:
+                    func()
+                
+                return True
+
+    #---------
+
+    def main_loop(self):
+        while self.loop:
+            # [1] check for input - restart loop if none
+            input_audio = ST.VoiceInput.get_phrase()
+            if not input_audio:
                 continue
+            input_time = datetime.now()
 
-            # [3] if main program didn't use input, pass it to the program/command currently in focus
-            #...
+            # [2] check if input is waking (either has wakeword or within wake timeout)
+            if ST.VoiceInput.transcribe_audio(input_audio, self.wake_words):    # if has wakeword
+                waking = True
+            elif self.last_wake_time and ST.validate_if_within_timeout(input_time, self.last_wake_time, self.wake_timeout): # if within timeout
+                waking = True
+            else:
+                waking = False
+
+            # [3] if waking, check input for commands
+            if waking:
+                self.last_wake_time = datetime.now()
+                input_keyword_text = ST.VoiceInput.transcribe_audio(input_audio, self.command_keyword_list) # transcribe audio featuring all possible commands keyboards
+                if input_keyword_text:
+                    command = self.check_spawn_commands(input_keyword_text)
+                    if command:
+                        # do command
+
+            # [4] if not waking, send input to currently in focus program
+            else:
+                pass
+
             
             # append command stuff to the log
 
-    @classmethod
-    def run(cls):
+    def run(self):
         ST.TerminalOutput.nl_print('loading...')
-        cls.loop = True
+        self.loop = True
         # start voice transcriber
         ST.VoiceInput.start_listening()
         # start main loop in new thread
-        main_t = Thread(target=cls.main_loop, daemon=True)
+        main_t = Thread(target=self.main_loop, daemon=True)
         main_t.start()
         # start GUI
         ST.GUI_tk.run_GUI()         # must be called from main thread, will persist
 
-    @classmethod
-    def shutdown(cls):
+    def shutdown(self):
         ST.TerminalOutput.nl_print('shutting down...')
-        cls.loop = False
+        self.loop = False
         ST.VoiceInput.stop_listening()
-        for prog in cls.progs:
-            prog.shutdown()
+        for prog in self.progs:
+            self.terminate_program(prog.id)
         ST.GUI.end_GUI()
-
-
-#---------
-
-def spawn_sub_program(program_object, name:str):
-    ST.TerminalOutput.nl_print(f'starting sub_program: "{name}"')
-    prog = program_object(name)                     # instatiate the object
-    prog_id = prog.id
-    progs.update({prog_id: prog})                   # add object to sub_progs list
-
-def terminate_sub_program(id:str):
-    prog = progs.get(id)
-    prog.shutdown()
-    progs.pop(id)
-
-def check_if_waking(input):
-    text = input.get('text')
-    time = input.get('time')
-
-    # [1] first validate input for command activation
-    # A. validate that either the phrase contains a wake word, or it happened within the wake_timeout
-    if ST.GeneralTools.match_keywords(('app_command_word', ), text):
-        pass
-    elif last_wake_time and ST.GeneralTools.validate_if_within_timeout(time, last_wake_time, wake_timeout):
-        pass
-    else:
-        return
-    
-    # if all the above conditions are met, then set the wake time to now, and return True
-    last_wake_time = datetime.now()
-    return True
-
-# check conditions, spawn commands
-def check_spawn_commands(text):
-    # [2] try getting action from input
-    for command in input_command_reference:
-        name = command.get('name')
-        conditions = command.get('conditions')
-        command = command.get('command')
-        
-        # check each of the comand's conditions - if any fail, then break the loop
-        for condition in conditions:
-            condition_func = condition[0]
-            condition_arg = condition[1]
-            passed = condition_func(condition_arg, text)
-            if not passed:
-                break
-        
-        # if all of the conditions pass, then do the action
-        if passed:
-            func = command[0]
-            args = command[1]
-
-            ST.TerminalOutput.nl_print(f'starting command: {name}')
-            last_wake_time = None      # reset wake time
-
-            if args:
-                # check if there is more than one argument, by seeing if args is an iterable
-                if hasattr(args, '__iter__'):
-                    func(*args)
-                else:
-                    func(args)
-            else:
-                func()
-            
-            return True
-
-#---------
-
-
-
-
-
-
 
 #-------------------------------
 
 if __name__ == "__main__":
-    run()
+    #m = MainProgram()
+    #m.run()
     print('goodbye!')
--- system_tools.py
+++ system_tools.py
@@ -2,22 +2,30 @@
 from time import sleep
 from datetime import datetime
 from threading import Lock
+from functools import wraps
+from typing import Callable, ParamSpec, TypeVar
 
 from external_scripts import stt_engine, play_rec_audio, nodes, GUI_tk
 
 #-------------------------------
 
+T = TypeVar('T')
+P = ParamSpec('P')
+
 class SharedResourceWrapper:
     def __init__(self):
         self.mutex = Lock()
 
-    def __call__(self, func):
-        def wrapper(*args,**kwargs):
+    def __call__(self, func:Callable[P, T]) -> Callable[P, T]:
+        @wraps(func)
+        def wrapper(*args:P.args, **kwargs:P.kwargs) -> T:
             with self.mutex:
-                func(*args,**kwargs)
-        return wrapper    
+                result = func(*args,**kwargs)
+            return result
+        return wrapper
 
 #-------------------------------
+# General Tools
 
 class SharedSingleItemContainer:
     def __init__(self):
@@ -35,99 +43,73 @@ class SharedSingleItemContainer:
             self.__item = None  
             return item
 
+TIME_STR_FRMT_1 = '%Y%m%d%H%M%S%f'
+KEYWORDS = {                # even single word combos MUST be tuples - don't foget the comma
+    'app_command_word': ('computer', ),
+    'get':      ('get', 'return', 'retrieve', 'show', 'display', 'read'),
+    'search':   ('search', 'find', 'seek', 'look'),
+    'create':   ('create','make', 'new'),   # 'write', 'start', 'compose'
+    'exit':     ('exit', 'shutdown', 'shut down', 'terminate', 'stop', 'bye', 'goodbye', 'good bye'),
+    'end':      ('end', 'finish', 'complete'),
+    'app':      ('app', 'application', 'program', 'computer'),
+    'note':     ('note', 'text', 'entry', 'page'),
+    'task':     ('task', 'todo', 'to-do'),
+    'recent':   ('recent', 'latest', 'last'),
+    'current':  ('current', 'present'),
+    'today':    ('today', 'todays', "today's")
+}
+
+# check if the specified keywords are present in text
+def match_keywords(keyword_names:tuple, text:str):
+    match_count = 0
+    for key in keyword_names:
+        keywords = KEYWORDS.get(key)
+        for word in keywords:
+            if word in text.lower():
+                match_count += 1
+                break
+    # only return True if each keyword group gets at least one match
+    if match_count >= len(keyword_names):
+        return True
+
+def validate_if_within_timeout(current_time:datetime, last_time:datetime, timeout):
+    if (current_time - last_time).seconds <= timeout:
+        return True
+
 #-------------------------------
-
-class GeneralTools:
-    TIME_STR_FRMT_1 = '%Y%m%d%H%M%S%f'
-    KEYWORDS = {                # even single word combos MUST be tuples - don't foget the comma
-        'app_command_word': ('computer', ),
-        'get':      ('get', 'return', 'retrieve', 'show', 'display', 'read'),
-        'search':   ('search', 'find', 'seek', 'look'),
-        'create':   ('create','make', 'new'),   # 'write', 'start', 'compose'
-        'exit':     ('exit', 'shutdown', 'shut down', 'terminate', 'stop', 'bye', 'goodbye', 'good bye'),
-        'end':      ('end', 'finish', 'complete'),
-        'app':      ('app', 'application', 'program', 'computer'),
-        'note':     ('note', 'text', 'entry', 'page'),
-        'task':     ('task', 'todo', 'to-do'),
-        'recent':   ('recent', 'latest', 'last'),
-        'current':  ('current', 'present'),
-        'today':    ('today', 'todays', "today's")
-    }
-
-    # check if the specified keywords are present in text
-    @classmethod
-    def match_keywords(cls, keyword_names:tuple, text:str):
-        match_count = 0
-        for key in keyword_names:
-            keywords = cls.KEYWORDS.get(key)
-            for word in keywords:
-                if word in text.lower():
-                    match_count += 1
-                    break
-        # only return True if each keyword group gets at least one match
-        if match_count >= len(keyword_names):
-            return True
-
-    def validate_if_within_timeout(current_time:datetime, last_time:datetime, timeout):
-        if (current_time - last_time).seconds <= timeout:
-            return True
-
 
 class VoiceInput:
     Vox = stt_engine.VoiceToText()
 
-    @classmethod
-    def start_listening(cls):
-        cls.Vox.start_listening()
-    
-    @classmethod
-    def stop_listening(cls):
-        cls.Vox.stop_listening()
-
-    @classmethod
-    def get_phrase(cls):
-        cls.Vox.get_audio_phrase()
-
-    @classmethod
-    def transcribe_audio(cls):
-        cls.Vox.transcribe_audio()
+    start_listening = Vox.start_listening
+    stop_listening = Vox.stop_listening
+    get_audio_phrase = Vox.get_audio_phrase
+    transcribe_audio = Vox.transcribe_audio
 
 
 class TerminalOutput():
-    wrap = SharedResourceWrapper()
+    __wrap = SharedResourceWrapper()
 
-    @wrap
-    def nl_print(message:str=None):
+    @__wrap
+    def nl_print(message:str):
         print('\n' + message)
 
 
 class AudioOutput:
-    wrap = SharedResourceWrapper()
+    __wrap = SharedResourceWrapper()
 
 
 class GUI:
-    wrap = SharedResourceWrapper()
+    __wrap = SharedResourceWrapper()
 
-    def start_GUI():
-        GUI_tk.run_GUI()
-    
-    def end_GUI():
-        GUI_tk.terminate_GUI()
-    
-    @wrap
-    def output_to_mainview():
-        GUI_tk.append_to_mainview()
-    
-    @wrap
-    def clear_mainview(): 
-        GUI_tk.clear_mainview()
+    start_GUI = GUI_tk.run_GUI
+    end_GUI = GUI_tk.terminate_GUI
+    output_to_mainview = __wrap(GUI_tk.append_to_mainview)
+    clear_mainview = __wrap(GUI_tk.clear_mainview)
 
 
 class Storage:
     files = nodes.ReadWriteNodes()
-    wrap = SharedResourceWrapper()
+    __wrap = SharedResourceWrapper()
 
-    @classmethod
-    @wrap
-    def create_node(cls):
-        cls.files.create_node()
+    create_node = __wrap(files.create_node)
